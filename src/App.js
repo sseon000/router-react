d0e70f1e13c9b4060f1fa53baff67a84290ea124
bugNick <rlatjsgur000@naver.com>
2022-09-10 15:56:54 +0900

    router first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import { Routes } from "react-router";
+import About from "./About";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
