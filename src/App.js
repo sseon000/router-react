9d612cbd7b9b99df61e148b3248017d1af818901
bugNick <rlatjsgur000@naver.com>
2022-09-13 20:29:12 +0900

    라우터 부가기능

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes } from "react-router";
 import About from "./About";
 import Home from "./Home";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -18,12 +19,16 @@ function App() {
         <li>
           <Link to="/profiles">프로필 목록</Link>
         </li>
+        <li>
+          <Link to="/history">예제</Link>
+        </li>
       </ul>
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profiles/*" element={<Profiles />} />
+        <Route path="/history" element={<HistorySample />} />
       </Routes>
     </div>
   );
